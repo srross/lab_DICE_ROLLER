13ff23288aea4958ba9903495db28d5a14408064
SoniaRoss <srross99@gmail.com>
2022-02-08 09:02:21 -0500

    initial commit

---
+++ DICE_ROLLER/DiceRoller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DICE_ROLLER
+{
+    public class DiceRoller
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Luck Be A Lady!");
+        }
+    }
+}
